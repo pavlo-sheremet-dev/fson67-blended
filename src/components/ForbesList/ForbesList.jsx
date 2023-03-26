d9eeccd5c115ab46fcfc481b9ba15df26598fce2
Danilo Korol <777danilkorol777@gmail.com>
2023-03-26 12:49:22 +0300

    task-3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -8,6 +8,29 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({ list }) => {
+  return (
+    <LeaderBoard>
+      <BoardHeader>
+        <BoardTitle>
+          <TitleTop>Forbes</TitleTop>
+          <TitleBottom>Leader board</TitleBottom>
+        </BoardTitle>
+      </BoardHeader>
+
+      <LeaderBoardProfiles>
+        {list.map(item => {
+          return (
+            <ForbesListItem
+              avatar={item.avatar}
+              capital={item.capital}
+              name={item.name}
+              isIncrease={item.isIncrease}
+              key={item.id}
+            />
+          );
+        })}
+      </LeaderBoardProfiles>
+    </LeaderBoard>
+  );
 };
